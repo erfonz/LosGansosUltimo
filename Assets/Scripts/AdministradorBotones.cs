ce5a9114854ab6979aed02e0cc693c1ef6e41c0f
Furybe <curibe895@unab.edu.co>
2018-09-28 01:21:06 -0500

    Interfaz de inicio primer intento

---
+++ Assets/Scripts/AdministradorBotones.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdministradorBotones : MonoBehaviour
+{
+    // Abrir escena de seleccion de personajes.
+    public void botonSelectPersonajes()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Scene002_Seleccion_Personajes");
+    }
+
+    // Abrir escena de opciones.
+    public void botonOpcion()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Scene003_Opciones");
+    }
+    //Salir del juego
+    public void Salida()
+    {
+        UnityEngine.Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Sound_Button.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_Button : MonoBehaviour {
+
+    public AudioSource fuente;
+    public AudioClip clip;
+	// Use this for initialization
+	void Start () {
+        fuente.clip = clip;
+	}
+	
+	public void Reproducir()
+    {
+        fuente.Play();
+    }
+}
